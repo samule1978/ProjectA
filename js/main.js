b404aa37e368b00d1e74ab70664ca1d53d84fb37
Samule Ghurry <samule.ghurry@coxauto.co.uk>
2021-03-12 15:06:18 +0000

    1.2 Dynamic FavIcon

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+let switchFavIcon = (state) => {
+	const favIconLight = "img/SG78Favicon.png";
+	const favIconDark = "img/SG78FaviconDark.png";
+
+	var favIcon = document.querySelector("link[rel~='icon']");
+	if (!favIcon) {
+		favIcon = document.createElement("link");
+		favIcon.rel = "icon";
+		document.getElementsByTagName("head")[0].appendChild(favIcon);
+	}
+	favIcon.href = state == "light" ? favIconLight : favIconDark;
+};
+
 let switchMode = (btn) => {
 	btn.classList.toggle("on");
-	["mode--light", "mode--dark"].map((mode) =>
-		document.body.classList.toggle(mode)
+	["light", "dark"].map((state) => {
+		document.body.classList.toggle(`mode--${state}`);
+	});
+
+	switchFavIcon(
+		document.body.classList.contains("mode--light") ? "light" : "dark"
 	);
 };
+
 const modeBtn = document.querySelector(".switch__btn");
 modeBtn.addEventListener("click", () => switchMode(modeBtn));
-
-let toggleOnOff = (element) => {
-	element.classList.toggle("on");
-};
-//const radioBtns = [...document.querySelectorAll(`[class^="--radio"]`)];
-//radioBtns.addEventListener("click", () => toggleOnOff(radioBtn));
